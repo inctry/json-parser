ce591ae356f7f5dc837f1812aabfe00dbbf1769a
Inctry <inctry@outlook.com>
2020-07-25 17:26:29 +0800

    Clion initial

---
+++ cmake-build-debug/CMakeFiles/json_parser.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/json_parser.dir/main.cpp.obj"
+  "json_parser.exe"
+  "json_parser.exe.manifest"
+  "json_parser.pdb"
+  "libjson_parser.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/json_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
